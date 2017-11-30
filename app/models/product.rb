5a7a0ba9766f40d294b17aa8c0da9efb3fff87e3
struthersdan <struthersdan@gmail.com>
2017-11-29 21:35:19 -0600

    added product model

---
+++ app/models/product.rb
@@ -0,0 +1,12 @@
+class Product < ApplicationRecord
+  class Product < ApplicationRecord
+    belongs_to :category
+    has_many :order_items
+    mount_uploader :image, ImageUploader
+
+    def self.search(search)
+      where("name LIKE ? ", "%#{search}%")
+    end
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171130030656) do
+ActiveRecord::Schema.define(version: 20171130033224) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,16 @@ ActiveRecord::Schema.define(version: 20171130030656) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.decimal "price"
+    t.bigint "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_products_on_category_id"
+  end
+
   create_table "provinces", force: :cascade do |t|
     t.string "name"
     t.decimal "PST"
@@ -77,4 +87,5 @@ ActiveRecord::Schema.define(version: 20171130030656) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "products", "categories"
 end
